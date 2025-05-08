fe1b3fe53a54b08e0812cb0a68253a7ac93b0d36
migr-kea <migr0002@stud.kea.dk>
2025-05-08 17:51:17 +0200

    Update app.py
    Fikset app.py:
    
    Man skal ikke længere skrive /index.html for at kunne tilgå siden, og URL linjen viser ikke længere .html på hver underside.

--- app.py
+++ app.py
@@ -6,19 +6,19 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about.html')
+@app.route('/about.')
 def about():
     return render_template('about.html')
 
-@app.route('/contact.html')
+@app.route('/contact')
 def contact():
     return render_template('contact.html')
 
-@app.route('/services.html')
+@app.route('/services')
 def gallery():
     return render_template('services.html')
 
-@app.route('/booking.html')
+@app.route('/booking')
 def booking():
     return render_template('booking.html')
 
