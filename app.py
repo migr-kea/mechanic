a3ea01a7d8c4cf3b2d36a07979ce363db60a4996
migr-kea <migr0002@stud.kea.dk>
2025-05-23 11:50:45 +0200

    small fixes

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ class Booking(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
+    nummerplade = db.Column(db.String(7), nullable=False)
+    telefon = db.Column(db.String(11), nullable=False)
     timeslot = db.Column(db.String(20), nullable=False)
     date = db.Column(db.Date, nullable=False)
 
@@ -54,13 +56,15 @@ def booking():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
+        nummerplade = request.form['nummerplade']
+        telefon = request.form['telefon']
         selection = request.form['datetime_slot']  # e.g. "2025-05-13|08:00"
         selected_date_str, timeslot = selection.split('|')
         selected_date = datetime.strptime(selected_date_str, '%Y-%m-%d').date()
 
         # Prevent double booking
         if not Booking.query.filter_by(date=selected_date, timeslot=timeslot).first():
-            db.session.add(Booking(name=name, email=email, timeslot=timeslot, date=selected_date))
+            db.session.add(Booking(name=name, email=email, nummerplade=nummerplade, telefon=telefon, timeslot=timeslot, date=selected_date))
             db.session.commit()
             return redirect(url_for('booking'))
 
